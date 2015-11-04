4b5d336a79676ca6b6ee9313d55c73cc9d5f733d
hotakasaito <teengenerate@gmail.com>
2015-11-04 16:42:39 +0900

    v0.0.3

--- run.sh
+++ run.sh
@@ -2,33 +2,33 @@
 set +e
 
 cd $HOME
-if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_APP_NAME" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_APP_NAME" ]
 then
     fail "Missing or empty option APP_NAME, please check wercker.yml"
 fi
 
-if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_ENV_NAME" ]
 then
     fail "Missing or empty option ENV_NAME, please check wercker.yml"
 fi
 
-if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_KEY" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_KEY" ]
 then
     fail "Missing or empty option KEY, please check wercker.yml"
 fi
 
-if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_SECRET" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_SECRET" ]
 then
     fail "Missing or empty option SECRET, please check wercker.yml"
 fi
 
-if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_REGION" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_REGION" ]
 then
     warn "Missing or empty option REGION, defaulting to us-west-2"
-    WERCKER_ELASTIC_BEANSTALK_DEPLOY_REGION="us-west-2"
+    WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_REGION="us-west-2"
 fi
 
-if [ -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_DEBUG" ]
+if [ -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_DEBUG" ]
 then
     warn "Debug mode turned on, this can dump potentially dangerous information to log files."
 fi
@@ -37,8 +37,8 @@ AWSEB_CREDENTIAL_FILE="$HOME/.elasticbeanstalk/aws_credential_file"
 AWSEB_EB_CONFIG_FILE="$WERCKER_SOURCE_DIR/.elasticbeanstalk/config.yml"
 
 export AWS_CREDENTIAL_FILE=$AWSEB_CREDENTIAL_FILE
-export AWS_ACCESS_KEY_ID=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_KEY
-export AWS_SECRET_ACCESS_KEY=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_SECRET
+export AWS_ACCESS_KEY_ID=$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_KEY
+export AWS_SECRET_ACCESS_KEY=$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_SECRET
 
 echo "Printenv..."
 env
@@ -63,33 +63,35 @@ cd $WERCKER_SOURCE_DIR
 debug "Setting up credentials..."
 test -d $(dirname $AWSEB_CREDENTIAL_FILE) || mkdir $(dirname $AWSEB_CREDENTIAL_FILE)
 cat <<EOT > $AWSEB_CREDENTIAL_FILE
-AWSAccessKeyId=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_KEY
-AWSSecretKey=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_SECRET
+AWSAccessKeyId=$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_KEY
+AWSSecretKey=$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_SECRET
 EOT
 
 debug "Setting up eb config..."
 cat <<EOF > $AWSEB_EB_CONFIG_FILE
 branch-defaults:
   default:
-    environment: $WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME
+    environment: $WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_ENV_NAME
   $WERCKER_GIT_BRANCH:
-    environment: $WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME
+    environment: $WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_ENV_NAME
 global:
-  application_name: $WERCKER_ELASTIC_BEANSTALK_DEPLOY_APP_NAME
+  application_name: $WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_APP_NAME
   default_platform: 64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4
   default_region: ap-northeast-1
   profile: eb-cli
   sc: git
+deploy:
+  artifact: /tmp/${$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_APP_NAME}.zip
 EOF
 
-if [ -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_DEBUG" ]
+if [ -n "$WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_DEBUG" ]
 then
     debug "Dumping config file."
     cat $AWSEB_CREDENTIAL_FILE
     cat $AWSEB_EB_CONFIG_FILE
 fi
 
-/usr/local/bin/eb use $WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME || fail "EB is not working or is not set up correctly."
+/usr/local/bin/eb use $WERCKER_ELASTIC_BEANSTALK_GO_DEPLOY_ENV_NAME || fail "EB is not working or is not set up correctly."
 
 debug "Checking if eb exists and can connect."
 /usr/local/bin/eb status || fail "EB is not working or is not set up correctly."
